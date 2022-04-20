7713db7a0da03b394e59270b05f6ccf3275a0991
Grisha <grisha.pavlov2012@yandex.ru>
2022-04-20 18:48:04 +0300

    TEXT-3: В процессе изучения создания кастомного редактора текста

---
+++ Assets/_Development/Jayzzer/Scripts/CustomInputField.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine.EventSystems;
+
+namespace _Development.Jayzzer.Scripts
+{
+    public class CustomInputField : TMP_InputField
+    {
+        public override void OnUpdateSelected(BaseEventData eventData)
+        {
+            base.OnUpdateSelected(eventData);
+        }
+    }
+}
